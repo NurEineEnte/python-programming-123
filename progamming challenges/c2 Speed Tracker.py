05aef6feb56adc3d1b3dbd650ef26c865d22de41
NurEineEnte <ajubb7tq@yare-edu.org.uk>
2024-09-15 14:09:34 +0000

    again and again

--- progamming challenges/c2 Speed Tracker.py
+++ progamming challenges/c2 Speed Tracker.py
@@ -2,8 +2,13 @@ firsttime = "01234567"
 secondtime = "01234567"
 
 numberplate = input("please enter your number plate, two letters, two numbers then 3 letters like XX77 XXX")
-if len(numberplate) == 8 and numberplate[0].isalpha() and numberplate[1].isalpha() and numberplate[3].isnumeric() and numberplate[4].isnumeric() and numberplate[5].isalpha() and numberplate[6].isalpha() and numberplate[7].isalpha()
+if len(numberplate) == 8 and numberplate[0].isalpha() and numberplate[1].isalpha() and numberplate[2].isnumeric() and numberplate[3].isnumeric() and numberplate[5].isalpha() and numberplate[6].isalpha() and numberplate[7].isalpha():
     print("Valid")
+    platevalid = True
+else:
+    print("Invalid")
+    platevalid = False
+
 while len(firsttime) != 8 or firsttime[2] != ":" or firsttime[5] != ":":
     firsttime = input("input the time first camera was passed in 24 hr format HH:MM:SS")
 while len(secondtime) != 8 or secondtime[2] != ":" or secondtime[5] != ":":
