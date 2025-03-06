c41a24a348298e62e47bd0df6f5680888c1420db
NurEineEnte <ajubb7tq@yare-edu.org.uk>
2025-03-06 09:58:26 +0000

    sigma sigma sigma

---
+++ progamming challenges/c22 Simple life calculator/life calculator.py
@@ -0,0 +1,27 @@
+def VAT():
+    while True:
+        try:
+            price = float(input("input the price of the goods to find VAT"))
+        except:
+            print("input a price (float/decimal value)")
+        else:
+            break
+    taxedamount = 0.2 * price
+    print("the vat added is", taxedamount, "meaning the total is", (taxedamout+price))
+
+
+def incometax():
+    while True:
+        try:
+            income = float(input("input your annual untaxed income to calculate tax"))
+        except:
+            print("input your annual income (float/decimal value)")
+        else:
+            break
+    if income > 12570
+    personal_allowance = income - 
+
+def timestable():
+    while True:
+        try:
+            print()
